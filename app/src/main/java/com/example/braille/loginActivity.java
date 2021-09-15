aee6b958ea331b1a1771ab2856fa2e2c5740844f
MADHUMITHA J <saimitha068@gmail.com>
2021-09-15 20:33:11 +0530

    first update

---
+++ app/src/main/java/com/example/braille/loginActivity.java
@@ -0,0 +1,34 @@
+package com.example.braille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class loginActivity extends AppCompatActivity {
+
+    EditText username,password;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login=new Intent(loginActivity.this,MainActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
+    }
+}
